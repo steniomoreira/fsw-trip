18c1ed9908fcd2f6fc2942a585005571f3f776a4
steniomoreira <steniomoreiradev@gmail.com>
2023-07-12 22:46:27 -0300

    feat: added trip header component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,6 +1,5 @@
 import { prisma } from "@/lib/prisma"
-import Image from "next/image"
-import ReactCountryFlag from "react-country-flag";
+import { TripHeader } from "./components/TripHeader";
 
 type TripDetailsProps = {
   params: {
@@ -25,22 +24,7 @@ export default async function TripDetails({ params }: TripDetailsProps) {
 
   return (
     <div className="container mx-auto">
-      <div className="relative w-full h-[300px]">
-        <Image className="object-cover" src={trip.coverImage} alt={trip.name} fill />
-      </div>
-
-      <div className="flex flex-col p-5">
-        <h1 className="font-semibold text-xl text-primaryDarker">{trip.name}</h1>
-
-        <div className="flex items-center gap-1 my-1">
-          <ReactCountryFlag countryCode={trip.countryCode} svg/>
-          <p className="text-xs text-dark underline">{trip.location}</p>
-        </div>
-
-        <p className="text-xs text-dark">
-          <span className="text-primary font-medium">R$ {trip.pricePerDay.toString()}</span> por dia
-        </p>
-      </div>
+      <TripHeader trip={trip} />
     </div>
   )
 } 
