b949704c0963a001791deaa4818759cadf8e9f08
steniomoreira <steniomoreiradev@gmail.com>
2023-07-12 23:47:45 -0300

    feat: added trip description component

---
+++ src/app/trips/[tripId]/components/TripDescription.tsx
@@ -0,0 +1,12 @@
+interface TripDescriptionProps {
+  description: string
+}
+
+export function TripDescription({ description }: TripDescriptionProps) {
+  return (
+    <div className="flex flex-col p-5 lg:p-0">
+      <h2 className="font-semibold text-primaryDarker lg:text-xl">Sobre a viagem</h2>
+      <p className="text-xs leading-5 text-primaryDarker mt-1 lg:mt-5 lg:text-base lg:leading-7">{description}</p>
+    </div>
+  );
+}
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma"
 import { TripHeader } from "./components/TripHeader";
 import { TripReservation } from "./components/TripReservation";
+import { TripDescription } from "./components/TripDescription";
 
 type TripDetailsProps = {
   params: {
@@ -28,6 +29,7 @@ export default async function TripDetails({ params }: TripDetailsProps) {
       <TripHeader trip={trip} />
 
       <TripReservation trip={trip} />
+      <TripDescription description={trip.description} />
     </div>
   )
 } 
