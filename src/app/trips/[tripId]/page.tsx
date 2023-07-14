056519a5270e04f9301c3c1c2ab06993e6bda9f4
steniomoreira <steniomoreiradev@gmail.com>
2023-07-13 22:48:17 -0300

    feat: added location description in schema prisma

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -3,6 +3,7 @@ import { TripHeader } from "./components/TripHeader";
 import { TripReservation } from "./components/TripReservation";
 import { TripDescription } from "./components/TripDescription";
 import { TripHighlights } from "./components/TripHighlights";
+import { TripLocation } from "./components/TripLocation";
 
 type TripDetailsProps = {
   params: {
@@ -31,6 +32,7 @@ export default async function TripDetails({ params }: TripDetailsProps) {
       <TripReservation trip={trip} />
       <TripDescription description={trip.description} />
       <TripHighlights highlights={trip.highlights} />
+      <TripLocation location={trip.location} locationDescription={trip.locationDescription} />
     </div>
   )
 } 
