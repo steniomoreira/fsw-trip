e714e6c911024dffbc62283bf12c823d836569c8
steniomoreira <steniomoreiradev@gmail.com>
2023-07-13 22:46:07 -0300

    feat: added variant attribute on button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,18 @@
 import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-function Button({ className, ...props }: ComponentPropsWithoutRef<"button">) {
-  const _className = twMerge(
-    className,
-    "appearance-none rounded-lg bg-primary p-2 text-sm font-medium text-white shadow transition-all hover:bg-primaryDarker"
-  );
+interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
+  variant?: "primary" | "outlined" | "danger";
+}
+
+function Button({ className, variant = "primary", ...props }: ButtonProps) {
+  const variantClasses = {
+    primary: "bg-primary text-white hover:bg-primaryDarker",
+    outlined: "bg-transparent border-2 border-primary text-primary",
+    danger: "text-red-500 border-red-500 border hover:bg-red-600 bg-transparent hover:text-white",
+  };
+
+  const _className = twMerge(variantClasses[variant], "appearance-none rounded-lg p-2 text-sm font-medium shadow transition-all", className);
 
   return (
     <button className={_className} {...props}>
